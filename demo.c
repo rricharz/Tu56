6b9b00b04dc1fbe3f842ccf88ea9afe75a20fd98
Rene Richarz <rene.richarz@bluewin.ch>
2019-12-10 07:56:04 +0100

    status file tests added

---
+++ demo.c
@@ -0,0 +1,76 @@
+/*
+ * demo.c
+ *
+ * Runs a demo on the tu56 visual display of the DECtape TU56 front panel
+ * 
+ * for the Raspberry Pi and other Linux systems
+ * 
+ * Copyright 2019  rricharz
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+#define TSTATE_ONLINE		 1
+#define TSTATE_DRIVE1		 2
+#define TSTATE_BACKWARDS	 4
+#define TSTATE_SEEK			 8
+#define TSTATE_READWRITE	16
+
+static FILE *statusFile = 0;
+
+void setStatus(int status)
+// set the status bits in the status file
+// if status file is accessible
+{
+	
+	char *fname = "/tmp/tu56status";
+	
+	if (statusFile == 0) {
+		statusFile = fopen(fname, "w");
+	}
+
+	if (statusFile != 0) {
+		fseek(statusFile,0L,SEEK_SET);
+		putc(32 + status,statusFile);
+		fflush(statusFile);
+	}
+}
+
+int main(int argc, char **argv)
+{	
+
+	setStatus(0);
+	sleep(1);
+	setStatus(TSTATE_ONLINE | TSTATE_BACKWARDS | TSTATE_SEEK);
+	sleep(2);
+	setStatus(TSTATE_ONLINE | TSTATE_SEEK);
+	usleep(500000);
+	for (int i = 1; i < 10; i++) {
+		setStatus(TSTATE_ONLINE);
+		usleep(250000);
+		setStatus(TSTATE_ONLINE | TSTATE_READWRITE);
+		sleep(3);
+	}
+	setStatus(0);
+	fclose(statusFile);	
+	return 0;
+}
+
--- makefile
+++ makefile
@@ -2,7 +2,13 @@ LIBS = `pkg-config --libs gtk+-3.0`
 
 CFLAGS = `pkg-config --cflags gtk+-3.0`
 
-all: tu56
+all: tu56 demo demoread
 
 tu56: tu56.c
 	gcc -o tu56 tu56.c $(LIBS) $(CFLAGS)
+
+demo: demo.c
+	gcc -o demo demo.c
+	
+demoread: demoread.c
+	gcc -o demoread demoread.c
